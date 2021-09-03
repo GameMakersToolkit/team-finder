6da30316af81587850456fad00fa0a875ee9effa
WillDotWhite <willdotwhiteis@gmail.com>
2021-09-03 12:46:19 +0100

    Minor fixups for code review

--- api/src/main/kotlin/com/gmtkgamejam/models/TeamDeleteDto.kt
+++ api/src/main/kotlin/com/gmtkgamejam/models/TeamDeleteDto.kt
@@ -3,7 +3,7 @@ package com.gmtkgamejam.models
 import kotlinx.serialization.Serializable
 
 /**
- * This model is the payload for a Team report
+ * This model is the payload for a Team Delete action
  */
 @Serializable
 data class TeamDeleteDto (
