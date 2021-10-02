8621997e5b88b33a0aa8908eba6550677035414e
Dallon Feldner <dallon.feldner@gmail.com>
2021-10-02 17:19:52 -0500

    Remove unnecessary console.log

--- ui/src/pages/Admin/UserView.tsx
+++ ui/src/pages/Admin/UserView.tsx
@@ -12,7 +12,6 @@ export interface User {
 export const UserView: React.FC<{user:User}> = ({user}) => {
   const buttonStyling = "bg-blue-500 w-full py-2 mb-3 rounded color-red"
 
-  console.log(user)
   return (
     <>
       <div className="inline-block" style={{width: "75%", verticalAlign: "top"}}>
