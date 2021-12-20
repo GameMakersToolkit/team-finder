01d5fea2dc67d4fe44970b18d3565beb6e8219ea
WillDotWhite <willdotwhiteis@gmail.com>
2021-12-20 14:58:19 +0000

    Update UI to use new endpoint and refactor images to use non-bitwise ID

--- ui/src/test-utils/api-mocks/handlers.ts
+++ ui/src/test-utils/api-mocks/handlers.ts
@@ -7,7 +7,7 @@ export const handlers = [
   rest.get("/spinner.svg", (req, res, ctx) => res(ctx.status(200))),
   rest.get("/SkillsetIcons/:file", (req, res, ctx) => res(ctx.status(200))),
 
-  rest.get(`${baseUrl}/teams`, (req, res, ctx) =>
+  rest.get(`${baseUrl}/posts`, (req, res, ctx) =>
     res(ctx.status(200), ctx.json(teamsFixtures.teamList))
   ),
 ];
--- ui/src/utils/Skillsets.ts
+++ ui/src/utils/Skillsets.ts
@@ -1,46 +1,24 @@
 export interface Skillset {
-  /**
-   * Bitwise ID (read: power of two) that can uniquely
-   * identify this specific role from all roles stored as aggregate bitwise int
-   */
-  id: number;
+  id: string;
   name: string;
 }
 
 export const skillsets: Skillset[] = [
-  { id: 1, name: "2D Art" },
-  { id: 2, name: "3D Art" },
-  { id: 4, name: "Code" },
-  { id: 8, name: "Design/\nProduction" },
-  { id: 16, name: "Sound/\nMusic" },
-  { id: 32, name: "Testing/\nSupport" },
-  { id: 128, name: "Team Lead" },
-  { id: 64, name: "Other" },
+  { id: "ART_2D",             name: "2D Art" },
+  { id: "ART_3D",             name: "3D Art" },
+  { id: "CODE",               name: "Code" },
+  { id: "DESIGN_PRODUCTION",  name: "Design/\nProduction" },
+  { id: "SOUND_MUSIC",        name: "Sound/\nMusic" },
+  { id: "TESTING_SUPPORT",    name: "Testing/\nSupport" },
+  { id: "TEAM_LEAD",          name: "Team Lead" },
+  { id: "OTHER",              name: "Other" },
 ];
 
 /**
- * Get all roles for a given Role ID (as an combined view of all roles, by the sum of the IDs)
+ * Convert between enum representation from DB and human-readable string
  */
-export const getSkillsets = (bitwiseSkillsetId: number): Skillset[] => {
-  return skillsets.filter((role) =>
-    roleIsInBitwiseSkillsetMask(bitwiseSkillsetId, role.id)
+export const getSkillsets = (skillsPossessed: string[]): Skillset[] => {
+  return skillsPossessed.map(skillPossessed =>
+      skillsets.filter(skill => skill.id === skillPossessed)[0]
   );
-};
-
-/**
- * Does the aggregated collection of all roles contain the given role ID?
- *
- * For ease of querying the DB, a collection of Roles is stored as a single int.
- * This int is the sum of all Roles which are active for that Team/Jammer
- *  (e.g. 2dArt+Code+Sound = 1+4+16 = 21)
- *
- * We can check if a given role is in the aggregated ("bitwise") role ID by doing
- * an AND operation on the two numbers when represented as binary; if the resulting
- * value of the AND is the roleId we passed in, then that bitwiseRoleId contains that role.
- */
-const roleIsInBitwiseSkillsetMask = (
-  bitwiseRoleId: number,
-  roleId: number
-): boolean => {
-  return (bitwiseRoleId & roleId) == roleId;
 };
