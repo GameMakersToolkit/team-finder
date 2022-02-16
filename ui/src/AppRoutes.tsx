38f1017acf21f06021852ea4641720291c2da5de
Dallon Feldner <dallon.feldner@gmail.com>
2022-02-16 17:35:18 -0600

    Test auth path

--- ui/src/AppRoutes.tsx
+++ ui/src/AppRoutes.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
+import { TestAuthenticatedPage } from "./pages/TestAuthenticatedPage/TestAuthenticatedPage";
 
 export const AppRoutes: React.FC = () => (
   <Routes>
     <Route path="/" element={<Home />} />
+    <Route path="/test-auth" element={<TestAuthenticatedPage />} />
     {/* TODO: replace with a proper Not Found page */}
     <Route path="*" element={<p>u wot m8</p>} />
   </Routes>
---
+++ ui/src/pages/TestAuthenticatedPage/TestAuthenticatedPage.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export function TestAuthenticatedPage(): React.ReactElement {
+  return <div>are ya logged in</div>;
+}
