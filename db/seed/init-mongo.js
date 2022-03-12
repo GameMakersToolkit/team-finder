16ae1026403919e25abe66d9b08811b265077721
WillDotWhite <willdotwhiteis@gmail.com>
2022-03-12 13:41:42 +0000

    Try to keep persistent index after first TTL run

--- db/seed/init-mongo.js
+++ db/seed/init-mongo.js
@@ -3,5 +3,9 @@
 db.auth("root", "example")
 db = db.getSiblingDB('team-finder')
 
+// Tear down existing auth collection context - sometimes we lose the index otherwise (for some reason)
+db.getCollection('auth').remove({})
+db.getCollection('auth').drop()
+
 // Create TTL for auth records
 db.getCollection('auth').createIndex( { "expiry": 1 }, { expireAfterSeconds: 10 } )
