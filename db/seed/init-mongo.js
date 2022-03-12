a5513ab5fcffd112e5a8de625477581c4cffac4d
WillDotWhite <willdotwhiteis@gmail.com>
2022-03-12 13:52:10 +0000

    Clear auth records after reasonable expiry timeframe

--- db/seed/init-mongo.js
+++ db/seed/init-mongo.js
@@ -8,4 +8,8 @@ db.getCollection('auth').remove({})
 db.getCollection('auth').drop()
 
 // Create TTL for auth records
-db.getCollection('auth').createIndex( { "expiry": 1 }, { expireAfterSeconds: 10 } )
+// The expiry field tracks the expiry of the accessToken, which is 10 minutes from issue;
+// We want to clear the record itself (including long-lived refreshToken) only when the token is no longer usable
+// Discord doesn't provide information on how long the refreshToken is valid for, but it's claimed up to 7 days
+// We use 1 day as a happy medium, as 24 hours since last activity seems like a reasonable amount of time to re-auth
+db.getCollection('auth').createIndex( { "expiry": 1 }, { expireAfterSeconds: 86400 } )
