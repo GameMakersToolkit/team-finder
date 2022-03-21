c307f6684b88b615848422abf444122f046b2135
Dallon Feldner <dallon.feldner@gmail.com>
2022-03-20 21:05:52 -0500

    actually test it lol

--- ui/src/model/skillIcons.ts
+++ ui/src/model/skillIcons.ts
@@ -1,6 +1,6 @@
 import type { Skill } from "./skill";
 
-const skillIcons = import.meta.globEager("./skill-icons/*.svg");
+const skillIcons = import.meta.globEager("./skill-icons-assets/*.svg");
 
 export function getSkillIcon(skill: Skill) {
   return skillIcons[`./skill-icons-assets/${skill}.svg`].default;
