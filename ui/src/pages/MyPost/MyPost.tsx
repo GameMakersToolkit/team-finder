4e1f6cb7aa740107498d20289be7129717b3e552
Dallon Feldner <dallonfdev@gmail.com>
2022-04-09 16:46:53 -0500

    Basic description field

--- ui/src/pages/MyPost/MyPost.tsx
+++ ui/src/pages/MyPost/MyPost.tsx
@@ -1,11 +1,44 @@
 import * as React from "react";
+import { Button } from "../../components/Button";
 import { useMyPostQuery } from "../../queries/my-post";
 import { useEnsureLoggedIn } from "../../utils/useEnsureLoggedIn";
 
 export const MyPost: React.FC = () => {
-  const userInfoQuery = useEnsureLoggedIn();
-
+  useEnsureLoggedIn();
   const myPostQuery = useMyPostQuery();
 
-  return <pre>{JSON.stringify(myPostQuery.data)}</pre>;
+  // summary
+  // description
+  // skillsPossessed
+  // skillsSought
+  // preferredTools
+  // availability
+  // timezoneStr
+  // languages
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form
+      className="container mx-auto max-w-screen-xxl p-1"
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor="description" className="text-lg">
+        Write a brief summary of what you're looking for that isn't covered by
+        the rest of the form
+      </label>
+      <textarea
+        id="description"
+        rows={5}
+        className="border-white border-2 bg-darkbg w-full py-1 px-2 mt-4"
+      />
+      <div className="flex mt-4 justify-end">
+        <Button type="submit" variant="primary">
+          Save
+        </Button>
+      </div>
+    </form>
+  );
 };
