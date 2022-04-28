d241b8890dffe9a5d52ff65b0800624e2e004bca
Dallon Feldner <dallonfdev@gmail.com>
2022-04-28 08:06:16 -0500

    Fix lint error

--- ui/src/pages/TestAuthenticatedPage/TestAuthenticatedPage.tsx
+++ ui/src/pages/TestAuthenticatedPage/TestAuthenticatedPage.tsx
@@ -5,7 +5,7 @@ export function TestAuthenticatedPage(): React.ReactElement | null {
   const userInfo = useEnsureLoggedIn();
 
   if (!userInfo.data) {
-    return <p>You'll be redirect to login with Discord shortly</p>;
+    return <p>You&apos;ll be redirect to login with Discord shortly</p>;
   }
 
   return (
