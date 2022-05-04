2ea7227d598d4e2f1e8989d79b4dbec871a2abdd
WillDotWhite <willdotwhiteis@gmail.com>
2022-05-04 11:21:50 +0100

    Standardise format of DB environment variables to match hosting needs
    DigitalOcean passes in a managed DATABASE_URL string, with a frequently
    changing password. While it's possible to use the individual fields directly
    like we have been doing, the system doesn't encourage it.
    
    Using a single environment variable should also make DB work easier for devs,
    I hope!

--- api/src/main/kotlin/com/gmtkgamejam/koin/DatabaseModule.kt
+++ api/src/main/kotlin/com/gmtkgamejam/koin/DatabaseModule.kt
@@ -6,22 +6,7 @@ import org.litote.kmongo.KMongo
 
 val DatabaseModule = module(createdAtStart = true) {
     single {
-        val user = Config.getString("secrets.database.user")
-        val password = Config.getString("secrets.database.password")
-        val host = Config.getString("secrets.database.host")
-        val port = Config.getString("secrets.database.port")
-        val protocol = Config.getString("secrets.database.protocol")
-        val certPath = Config.getString("secrets.database.certPath")
-
-        if (protocol == "mongodb") {
-            // Create simple connection
-            KMongo.createClient("$protocol://$user:$password@$host:$port")
-        } else {
-            // FIXME: None of this should need breaking out into config values,
-            //        but 'team-finder' and the replicaSet can be changed externally
-            val settings = "team-finder?authSource=admin&replicaSet=db-mongodb-1&tls=true&tlsCAFile=$certPath"
-            KMongo.createClient("$protocol://$user:$password@$host/$settings")
-        }
-
+        val url = Config.getString("secrets.database.url")
+        KMongo.createClient(url)
     }
 }
