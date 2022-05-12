e53c23b2347e0672ad53b6a8ed48a8044c2b4b31
WillDotWhite <willdotwhiteis@gmail.com>
2022-05-12 21:02:22 +0100

    Also Admin views lmao

--- ui/src/pages/Admin/Admin.tsx
+++ ui/src/pages/Admin/Admin.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import { useReportedPostsList } from "../../queries/admin";
 import { PostPreview } from "../../components/PostPreview";
+import { ViewOptions } from "../Home/components/ViewOptions";
 
 export const Admin: React.FC = () => {
   const reportedPosts = useReportedPostsList();
-  
+  const [showSkillText, setShowSkillText] = useState(true)
 
   return (
     <div className="container mx-auto max-w-screen-xxl p-1">
+      <ViewOptions showSkillText={showSkillText} setShowSkillText={setShowSkillText} />
+
       {reportedPosts.data && (
         <div className="mt-4">
           Reported posts: {reportedPosts.data.length} results found
@@ -19,7 +22,7 @@ export const Admin: React.FC = () => {
           post={post}
           className="mt-4"
           adminView={true}
-        />
+          showSkillText={showSkillText} />
       ))}
     </div>
   );
