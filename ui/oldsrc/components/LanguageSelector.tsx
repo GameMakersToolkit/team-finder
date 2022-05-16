e9d907a90c7e0740c5380f7003e35a8c7749e5ca
WillDotWhite <willdotwhiteis@gmail.com>
2022-05-16 10:03:22 +0100

    Remove unused code, let git handle time travel

--- ui/oldsrc/components/LanguageSelector.tsx
+++
@@ -1,5 +0,0 @@
-import { ArrayToRecord } from "../utils/ArrayToRecord";
-import { languages } from "../utils/LanguageData";
-
-
-export const languageSelectIndex: Record<string, string> = ArrayToRecord(languages, l => [l.code, l.display]);
--- ui/oldsrc/components/SelectableTeamName.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-export const SelectableTeamName: React.FC = ({ children }) => {
-  return <span className="select-all">{children}</span>;
-};
