0eac5518d382d8f740a55efd48d5835cc057f4ff
awildbrysen <9168041+awildbrysen@users.noreply.github.com>
2022-05-27 15:48:26 +0200

    Split /posts/favourites and /posts
    Have an endpoint that only shows the favourite posts

--- api/src/main/kotlin/com/gmtkgamejam/routing/PostRoutes.kt
+++ api/src/main/kotlin/com/gmtkgamejam/routing/PostRoutes.kt
@@ -1,6 +1,5 @@
-package com.gmtkgamejam.routing;
+package com.gmtkgamejam.routing
 
-import com.auth0.jwt.JWT
 import com.gmtkgamejam.enumFromStringSafe
 import com.gmtkgamejam.models.*
 import com.gmtkgamejam.services.AuthService
@@ -23,86 +22,77 @@ fun Application.configurePostRouting() {
     val service = PostService()
     val favouritesService = FavouritesService()
 
+    fun getFilterFromParameters(params: Parameters): List<Bson> {
+        val filters = mutableListOf<Bson>(PostItem::deletedAt eq null)
+
+        params["description"]?.split(',')
+            ?.filter(String::isNotBlank) // Filter out empty `&description=`
+            // The regex is the easiest way to check if a description contains a given substring
+            ?.forEach {
+                filters.add(
+                    or(
+                        PostItem::title regex it.toRegex(RegexOption.IGNORE_CASE),
+                        PostItem::description regex it.toRegex(RegexOption.IGNORE_CASE)
+                    )
+                )
+            }
+
+        params["skillsPossessed"]?.split(',')
+            ?.filter(String::isNotBlank) // Filter out empty `&skillsPossessed=`
+            ?.mapNotNull { enumFromStringSafe<Skills>(it) }
+            ?.map { PostItem::skillsPossessed contains it }
+            ?.let(filters::addAll)
+
+        params["skillsSought"]?.split(',')
+            ?.filter(String::isNotBlank) // Filter out empty `&skillsSought=`
+            ?.mapNotNull { enumFromStringSafe<Skills>(it) }
+            ?.map { PostItem::skillsSought contains it }
+            ?.let(filters::addAll)
+
+        params["tools"]?.split(',')
+            ?.filter(String::isNotBlank) // Filter out empty `&skillsSought=`
+            ?.mapNotNull { enumFromStringSafe<Tools>(it) }
+            ?.map { PostItem::preferredTools contains it }
+            ?.let(filters::addAll)
+
+        params["languages"]?.split(',')
+            ?.filter(String::isNotBlank) // Filter out empty `&languages=`
+            ?.map { PostItem::languages contains it }
+            ?.let { filters.add(or(it)) }
+
+        params["availability"]?.split(',')
+            ?.filter(String::isNotBlank) // Filter out empty `&availability=`
+            ?.mapNotNull { enumFromStringSafe<Availability>(it) }
+            ?.map { PostItem::availability eq it }
+            // Availabilities are mutually exclusive, so treat it as inclusion search
+            ?.let { filters.add(or(it)) }
+
+        val timezoneRange = params["timezones"]?.split('/')
+        if (timezoneRange != null && timezoneRange.size == 2) {
+            val timezoneStart: Int = timezoneRange[0].toInt()
+            val timezoneEnd: Int = timezoneRange[1].toInt()
+
+            if (timezoneStart < timezoneEnd) {
+                // UTC-2 -> UTC+2 should be: [-2, -1, 0, 1, 2]
+                filters.add(and(PostItem::timezoneOffset gte timezoneStart, PostItem::timezoneOffset lte timezoneEnd))
+            } else {
+                // UTC+9 -> UTC-9 should be: [9, 10, 11, 12, -12, -11, -10, -9]
+                filters.add(
+                    or(
+                        and(PostItem::timezoneOffset gte timezoneStart, PostItem::timezoneOffset lte 12),
+                        and(PostItem::timezoneOffset gte -12, PostItem::timezoneOffset lte timezoneEnd)
+                    )
+                )
+            }
+        }
+
+        return filters
+    }
+
     routing {
         route("/posts") {
             get {
                 val params = call.parameters
-
-                // All Posts found should be active
-                val filters = mutableListOf<Bson>(PostItem::deletedAt eq null)
-                val favouritesFilters = mutableListOf<Bson>()
-
-                params["description"]?.split(',')
-                    ?.filter ( String::isNotBlank ) // Filter out empty `&description=`
-                    // The regex is the easiest way to check if a description contains a given substring
-                    ?.forEach {
-                        filters.add(or(
-                            PostItem::title regex it.toRegex(RegexOption.IGNORE_CASE),
-                            PostItem::description regex it.toRegex(RegexOption.IGNORE_CASE)
-                        ))
-                    }
-
-                params["skillsPossessed"]?.split(',')
-                    ?.filter ( String::isNotBlank ) // Filter out empty `&skillsPossessed=`
-                    ?.mapNotNull { enumFromStringSafe<Skills>(it) }
-                    ?.map { PostItem::skillsPossessed contains it }
-                    ?.let ( filters::addAll )
-
-                params["skillsSought"]?.split(',')
-                    ?.filter ( String::isNotBlank ) // Filter out empty `&skillsSought=`
-                    ?.mapNotNull { enumFromStringSafe<Skills>(it) }
-                    ?.map { PostItem::skillsSought contains it }
-                    ?.let ( filters::addAll )
-
-                params["tools"]?.split(',')
-                    ?.filter ( String::isNotBlank ) // Filter out empty `&skillsSought=`
-                    ?.mapNotNull { enumFromStringSafe<Tools>(it) }
-                    ?.map { PostItem::preferredTools contains it }
-                    ?.let ( filters::addAll )
-
-                params["languages"]?.split(',')
-                    ?.filter ( String::isNotBlank ) // Filter out empty `&languages=`
-                    ?.map { PostItem::languages contains it }
-                    ?.let { filters.add(or(it)) }
-
-                params["availability"]?.split(',')
-                    ?.filter ( String::isNotBlank ) // Filter out empty `&availability=`
-                    ?.mapNotNull { enumFromStringSafe<Availability>(it) }
-                    ?.map { PostItem::availability eq it }
-                    // Availabilities are mutually exclusive, so treat it as inclusion search
-                    ?.let { filters.add(or(it)) }
-
-                // Timezones
-                val timezoneRange = params["timezones"]?.split('/')
-                if (timezoneRange != null && timezoneRange.size == 2) {
-                    val timezoneStart: Int = timezoneRange[0].toInt()
-                    val timezoneEnd: Int = timezoneRange[1].toInt()
-
-                    if (timezoneStart < timezoneEnd) {
-                        // UTC-2 -> UTC+2 should be: [-2, -1, 0, 1, 2]
-                        filters.add(and(PostItem::timezoneOffset gte timezoneStart, PostItem::timezoneOffset lte timezoneEnd))
-                    } else {
-                        // UTC+9 -> UTC-9 should be: [9, 10, 11, 12, -12, -11, -10, -9]
-                        filters.add(or(
-                            and(PostItem::timezoneOffset gte timezoneStart, PostItem::timezoneOffset lte 12),
-                            and(PostItem::timezoneOffset gte -12, PostItem::timezoneOffset lte timezoneEnd)
-                        ))
-                    }
-                }
-
-                // Favourited posts, _if_ user is logged in
-                var favouritePostIds = mutableListOf<Long>()
-                call.request.header("Authorization")?.substring(7)
-                    ?.let { JWT.decode(it) }
-                    ?.let { it.getClaim("id").asString() }
-                    ?.let { authService.getTokenSet(it) }
-                    ?.let { favouritesService.getFavouritesByUserId(it.discordId) }
-                    ?.also { favouritePostIds = it.postIds }
-                    ?.let { favouritesList ->
-                        favouritesList.postIds.map {
-                            favouritesFilters.add(and(PostItem::id eq it, PostItem::deletedAt eq null))
-                        }
-                    }
 
                 // Sorting
                 // TODO: Error handling
@@ -117,10 +107,7 @@ fun Application.configurePostRouting() {
                 // Pagination
                 val page = params["page"]?.toInt() ?: 1
 
-                val combinedFilter = or(and(filters), or(favouritesFilters)) // One and() call combines all filters into a single bool query
-
-                val posts = service.getPosts(combinedFilter, sort, page)
-                posts.map { it.isFavourite = favouritePostIds.contains(it.id) }
+                val posts = service.getPosts(and(getFilterFromParameters(params)), sort, page)
 
                 call.respond(posts)
             }
@@ -150,6 +137,36 @@ fun Application.configurePostRouting() {
                         ?.let { return@post call.respond(it) }
 
                     call.respondText("Post could not be created", status = HttpStatusCode.NotFound)
+                }
+
+                get("favourites") {
+                    val params = call.parameters
+                    val principal = call.principal<JWTPrincipal>()!!
+                    val id = principal.payload.getClaim("id").asString()
+
+                    val favourites = authService.getTokenSet(id)
+                        ?.let { favouritesService.getFavouritesByUserId(it.discordId) }
+
+                    // Sorting
+                    // TODO: Error handling
+                    val sortByFieldName = params["sortBy"] ?: "id"
+                    val sortByField = PostItem::class.memberProperties.first { prop -> prop.name == sortByFieldName }
+                    val sort = when (params["sortDir"].toString()) {
+                        "asc" -> ascending(sortByField)
+                        "desc" -> descending(sortByField)
+                        else -> ascending(sortByField)
+                    }
+
+                    // Pagination
+                    val page = params["page"]?.toInt() ?: 1
+
+                    val favouritesFilters = mutableListOf<Bson>()
+                    favourites?.postIds?.forEach {
+                        favouritesFilters.add(and(PostItem::id eq it, PostItem::deletedAt eq null))
+                    }
+
+                    val posts = service.getPosts(and(and(favouritesFilters), and(getFilterFromParameters(params))), sort, page)
+                    call.respond(posts)
                 }
 
                 route("/mine") {
