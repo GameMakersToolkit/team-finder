e1e399187b14627df7b97a32ca1d204e02d5b8cd
WillDotWhite <willdotwhiteis@gmail.com>
2022-06-04 12:44:19 +0100

    Rate limit user contacts for fun and education

--- api/src/main/kotlin/com/gmtkgamejam/routing/DiscordBotRoutes.kt
+++ api/src/main/kotlin/com/gmtkgamejam/routing/DiscordBotRoutes.kt
@@ -1,5 +1,6 @@
 package com.gmtkgamejam.routing
 
+import com.gmtkgamejam.Config
 import com.gmtkgamejam.bot.DiscordBot
 import com.gmtkgamejam.services.AuthService
 import io.ktor.application.*
@@ -8,12 +9,38 @@ import io.ktor.auth.jwt.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import org.koin.ktor.ext.inject
+import java.time.LocalDateTime
 
 
 fun Application.configureDiscordBotRouting() {
 
     val authService = AuthService()
     val bot: DiscordBot by inject()
+
+    val userIdMessageTimes: MutableMap<String, LocalDateTime> = mutableMapOf()
+    val userIdPerUserMessageTimes: MutableMap<Pair<String, String>, LocalDateTime> = mutableMapOf()
+
+    // User A needs to wait X seconds between messaging User B and User C
+    val userRateLimitTimeOutInSeconds = Config.getString("bot.userRateLimit").toLong()
+
+    // User A can't ping User B more than once per X seconds
+     val perUserTimeoutInSeconds = Config.getString("bot.perRecipientRateLimit").toLong()
+
+    fun isUserWithinRateLimit(userId: String): Boolean {
+        val currentDateTime = LocalDateTime.now()
+        val previousMessageDateTime = userIdMessageTimes[userId] ?: LocalDateTime.MIN
+
+        return !previousMessageDateTime.plusSeconds(userRateLimitTimeOutInSeconds).isAfter(currentDateTime)
+    }
+
+    fun isUserWithinPerUserTimeout(senderId: String, recipientId: String): Boolean {
+        val currentDateTime = LocalDateTime.now()
+
+        val specificRecipientKey = Pair(senderId, recipientId)
+        val previousMessageDateTime = userIdPerUserMessageTimes[specificRecipientKey] ?: LocalDateTime.MIN
+
+        return !previousMessageDateTime.plusSeconds(perUserTimeoutInSeconds).isAfter(currentDateTime)
+    }
 
     routing {
         authenticate("auth-jwt") {
@@ -23,12 +50,22 @@ fun Application.configureDiscordBotRouting() {
                     val principal = call.principal<JWTPrincipal>()!!
                     val id = principal.payload.getClaim("id").asString()
 
-                    authService.getTokenSet(id)
-                        // TODO: Validate/rate limit
-                        ?.let { bot.createContactUserPingMessage(params["recipientUserId"].toString(), it.discordId) }
-                        ?.let { return@post call.respond(it) }
+                    val tokenSet = authService.getTokenSet(id) ?: return@post call.respondText("lolno - auth")
 
-                    call.respondText("lolno")
+                    val senderId = tokenSet.discordId
+                    val recipientId = params["recipientUserId"].toString()
+                    if (!isUserWithinRateLimit(senderId)){
+                        return@post call.respondText("lolno - user rate limit")
+                    }
+
+                    if (!isUserWithinPerUserTimeout(senderId, recipientId)) {
+                        return@post call.respondText("lolno - recipient rate limit")
+                    }
+
+                    bot.createContactUserPingMessage(recipientId, senderId)
+                    userIdMessageTimes[senderId] = LocalDateTime.now()
+                    userIdPerUserMessageTimes[Pair(senderId, recipientId)] = LocalDateTime.now()
+                    return@post call.respond(it)
                 }
             }
         }
