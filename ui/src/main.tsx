a9fcacf1b8c6a308b6507851205449a7d12e5410
WillDotWhite <willdotwhiteis@gmail.com>
2022-09-06 11:05:49 +0100

    Add useEffect hook to allow page rerender on JamState change

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,31 +2,20 @@ import "focus-visible";
 import React from "react";
 import ReactDOM from "react-dom";
 import { ReactQueryDevtools } from "react-query/devtools";
-import { Context } from "../src/Context";
-import { importMetaEnv } from "../src/utils/importMeta";
-
-import { AppRoutes } from "./AppRoutes";
-import PageHeader from "./pages/PageHeader";
+import { Context } from "./Context";
+import { importMetaEnv } from "./utils/importMeta";
+import { App } from "./App";
 import "./index.css";
-import { Toaster } from "react-hot-toast";
-import { getJamState, JamState } from "./utils/jamState";
 
 if (importMetaEnv().PROD) {
   import("../src/utils/init-sentry");
 }
 
-const jamState: JamState = getJamState()
-
 ReactDOM.render(
   <React.StrictMode>
     <Context>
       <React.Suspense fallback={null}>
-        <PageHeader jamState={jamState} />
-        <AppRoutes jamState={jamState} />
-        <Toaster
-          position="bottom-center"
-          reverseOrder={false}
-        />
+        <App />
       </React.Suspense>
       <ReactQueryDevtools />
     </Context>
