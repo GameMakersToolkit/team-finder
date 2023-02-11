764f1d7e1ed841ccdfee2cc76e457bc0c662e8cd
awildbrysen <9168041+awildbrysen@users.noreply.github.com>
2023-02-11 20:22:07 +0100

    Bump Ktor version to 2.2.3
    Change the CORS import to the non-deprecated CORS plugin

--- api/src/main/kotlin/com/gmtkgamejam/Application.kt
+++ api/src/main/kotlin/com/gmtkgamejam/Application.kt
@@ -8,6 +8,7 @@ import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.cors.*
+import io.ktor.server.plugins.cors.routing.CORS
 import kotlinx.serialization.json.Json
 import org.koin.core.context.startKoin
 import org.koin.environmentProperties
