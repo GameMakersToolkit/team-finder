e9a64b589161356f5b35e3e49c1cdc725a9d529a
WillDotWhite <willdotwhiteis@gmail.com>
2023-05-27 20:20:45 +0100

    Set default tab to search input

--- ui/src/pages/Home/Home.tsx
+++ ui/src/pages/Home/Home.tsx
@@ -141,6 +141,7 @@ export const Home: React.FC = () => {
         <input
           id="descriptionFilter"
           type="text"
+          tabIndex="0"
           placeholder="Search for keywords in a post (e.g. 'retro', 'platformer', 'top down')"
           className="input px-2 py-1 w-full h-[36px]"
           value={description}
