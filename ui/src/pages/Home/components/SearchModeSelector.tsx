5985dd169f5825d36c3a15c06d514fa6840613c1
juve938383 <120019378+juve-938383@users.noreply.github.com>
2023-05-28 17:32:40 +0200

    Show default radio button selection

--- ui/src/pages/Home/components/SearchModeSelector.tsx
+++ ui/src/pages/Home/components/SearchModeSelector.tsx
@@ -40,6 +40,7 @@ export function SearchModeSelector({
                 type="radio"
                 role="radio"
                 aria-checked={value == option}
+                checked={value == option}
                 className={`text-white cursor-pointer p-2 mr-2 mb-2 `}
                 data-search-mode={option}
                 onClick={() => onChange(option)}
