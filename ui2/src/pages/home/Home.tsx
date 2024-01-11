5cde839d32000dac990e514d351fe05c943b7cfb
WillDotWhite <will.white01@bbc.co.uk>
2024-01-11 18:18:30 +0000

    Wire up bookmark button in header

--- ui2/src/pages/home/Home.tsx
+++ ui2/src/pages/home/Home.tsx
@@ -5,16 +5,29 @@ import {useEffect, useState} from "react";
 import {PostTile} from "../../common/components/PostTile.tsx";
 import {Onboarding} from "./components/Onboarding.tsx";
 import {SiteIntro} from "./components/SiteIntro.tsx";
+import {importMetaEnv} from "../../common/utils/importMeta.ts";
+import {useAuth} from "../../api/AuthContext.tsx";
 
 export const Home: React.FC = () => {
     const [searchParams, setSearchParams] = useSearchParams();
     const [posts, setPosts] = useState([])
+    const { token } = useAuth() ?? {};
 
     // Trigger API call every time query string changes
     // Not sure if we actually need react-query here, but I'm keeping it everywhere else for now to avoid unnecessary work
     useEffect(() => {
-        console.log("http://localhost:8080/posts?" + searchParams.toString())
-        fetch("http://localhost:8080/posts?" + searchParams.toString())
+        const path = searchParams.get('bookmarked') === "true" ? "posts/favourites" : "posts"
+        searchParams.delete('bookmarked')
+
+        const url = new URL(path + "?" + searchParams.toString(), importMetaEnv().VITE_API_URL)
+        const init: RequestInit = {method: "GET", headers: {"Content-Type": "application/json"}}
+
+        if (token) {
+            // @ts-ignore
+            init.headers['Authorization'] = `Bearer ${token}`
+        }
+
+        fetch(url, init)
             .then(res => res.json())
             .then(setPosts)
     }, [searchParams])
