c118f81a25d3a2cc6b0a9b89d4de368bcbda0d0b
WillDotWhite <will.white01@bbc.co.uk>
2024-01-18 17:11:45 +0000

    Display meaningful message when a user has no bookmarks

--- ui/src/pages/home/Home.tsx
+++ ui/src/pages/home/Home.tsx
@@ -10,13 +10,16 @@ import {Post} from "../../common/models/post.ts";
 
 export const Home: React.FC = () => {
     const [searchParams, setSearchParams] = useSearchParams();
-    const [posts, setPosts] = useState<Post[]>([])
+    const [posts, setPosts] = useState<Post[]>([]);
+    const [isViewingBookmarks, setIsViewingBookmarks] = useState<boolean>(searchParams.get('bookmarked') === "true");
     const { token } = useAuth() ?? {};
 
     // Trigger API call every time query string changes
     // Not sure if we actually need react-query here, but I'm keeping it everywhere else for now to avoid unnecessary work
     useEffect(() => {
-        const path = searchParams.get('bookmarked') === "true" ? "posts/favourites" : "posts"
+        const isOnlyBookmarked = searchParams.get('bookmarked') === "true"
+        const path = isOnlyBookmarked ? "posts/favourites" : "posts"
+        setIsViewingBookmarks(isOnlyBookmarked)
         searchParams.delete('bookmarked')
 
         const url = new URL(path + "?" + searchParams.toString(), import.meta.env.VITE_API_URL)
@@ -41,9 +44,27 @@ export const Home: React.FC = () => {
             <SearchFormWrapper searchParams={searchParams} setSearchParams={setSearchParams} />
 
             {posts?.length > 0
-                ? (<div className="c-post-tiles">{posts.map(post => <PostTile key={post.id} post={post} />)}</div>)
-                : (<h2 className="text-xl text-center">Please wait...</h2>)
+                ? <PostsToDisplay posts={posts} />
+                : <NoPostsToDisplay isViewingBookmarks={isViewingBookmarks} />
             }
         </main>
+    )
+}
+
+const PostsToDisplay: React.FC<{posts: Post[]}> = ({posts}) => {
+    return (
+        <div className="c-post-tiles">{posts.map(post => <PostTile key={post.id} post={post} />)}</div>
+    )
+}
+
+const NoPostsToDisplay: React.FC<{isViewingBookmarks: boolean}> = ({isViewingBookmarks}) => {
+    if (isViewingBookmarks) {
+        return (
+            <h2 className="text-xl text-center">You don't have any bookmarked posts</h2>
+        )
+    }
+
+    return (
+        <h2 className="text-xl text-center">Please wait...</h2>
     )
 }
