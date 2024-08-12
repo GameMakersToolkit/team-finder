9899c05fdc7adcf72b14baa21d202b163e0979de
WillDotWhite <will.white01@bbc.co.uk>
2024-08-12 21:19:00 +0100

    Tidy up countdown timer a bit

--- ui/src/pages/home/components/SiteIntro.tsx
+++ ui/src/pages/home/components/SiteIntro.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 const jamName = import.meta.env.VITE_JAM_NAME;
 const jamStartDate = new Date(import.meta.env.VITE_JAM_START);
+const jamEndDate = new Date(import.meta.env.VITE_JAM_END);
 
 // Set the date we're counting down to
 export const SiteIntro = () => {
@@ -18,6 +19,8 @@ export const SiteIntro = () => {
               <div className="flex flex-col justify-center mb-4 sm:m-0">
                   <p className="text-center">{`Welcome to the ${jamName} Team Finder!`}</p>
                   <p className="text-center">Create a post or search below to find a team.</p>
+                  <p>&nbsp;</p>
+                  <p className="text-center">The jam will run from<br /><span className="font-bold">August 16th 2024 at 6:00 PM (BST)</span> to<br /><span className="font-bold">August 20th 2024 at 6:00 PM (BST)</span></p>
               </div>
 
               <div className="text-center">
@@ -45,20 +48,21 @@ const CountdownSection = () => {
     countdown.minutes <= 0 &&
     countdown.seconds <= 0
   ) {
-    countdown.days = 0
-    countdown.hours = 0
-    countdown.minutes = 0
-    countdown.seconds = 0
+    return (
+        <>
+            <p className="text-center mb-2">We hope you enjoyed the jam!</p>
+        </>
+    )
   }
 
   return (
       <>
-      <p className="text-center mb-2">The jam starts in:</p>
-      <div className="flex justify-evenly bg-red-600 border-red-600 border-2 rounded-xl mx-4 px-2 py-3 text-4xl">
-          {countdown.days > 0 && <><span>{`${countdown.days.toString().padStart(2, '0')}`}</span><span>:</span></>}
-          {(countdown.days > 0 || countdown.hours > 0) && <><span>{`${countdown.hours.toString().padStart(2, '0')}`}</span><span>:</span></>}
-          {(countdown.days > 0 || countdown.hours > 0 || countdown.minutes > 0) && <><span>{`${countdown.minutes.toString().padStart(2, '0')} `}</span><span>:</span></>}
-          <span>{`${countdown.seconds.toString().padStart(2, '0')} `}</span>
+      <p className="text-center mb-2">The jam {countdown.reference}s in:</p>
+      <div className="flex justify-evenly bg-red-600 border-red-600 border-2 rounded-xl mx-4 px-2 py-3 text-3xl">
+          {countdown.days > 0 && <><span>{`${countdown.days.toString().padStart(2, '0')}`}<span className="block text-sm italic">days</span></span><span className="pt-1">:</span></>}
+          {(countdown.days > 0 || countdown.hours > 0) && <><span>{`${countdown.hours.toString().padStart(2, '0')}`}<span className="block text-sm italic">hours</span></span><span className="pt-1">:</span></>}
+          {(countdown.days > 0 || countdown.hours > 0 || countdown.minutes > 0) && <><span>{`${countdown.minutes.toString().padStart(2, '0')} `}<span className="block text-sm italic">minutes</span></span><span className="pt-1">:</span></>}
+          <span>{`${countdown.seconds.toString().padStart(2, '0')} `}<span className="block text-sm italic">seconds</span></span>
       </div>
     </>
   )
@@ -66,7 +70,12 @@ const CountdownSection = () => {
 
 const getCountdownComponents = (time: number) => {
     // Find the distance between now and the countdown date
-    const distance = jamStartDate.getTime() - time;
+    let distance = jamStartDate.getTime() - time;
+    let reference = "start";
+    if (distance < 0) {
+        distance = jamEndDate.getTime() - time;
+        reference = "end"
+    }
 
     // Time calculations for days, hours, minutes and seconds
     const weeks =   Math.floor(distance / (1000 * 60 * 60 * 24 * 7));
@@ -75,5 +84,5 @@ const getCountdownComponents = (time: number) => {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    return {weeks, days, hours, minutes, seconds};
+    return {weeks, days, hours, minutes, seconds, reference};
 }
