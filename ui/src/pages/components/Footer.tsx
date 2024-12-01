f3aaa7b71a377d94839f2c4379a163d9cf4ea8c1
WillDotWhite <will.white01@bbc.co.uk>
2024-12-01 12:12:10 +0000

    Update footer link for discord

--- ui/src/pages/components/Footer.tsx
+++ ui/src/pages/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC = () => {
     const footerImages = [
         // {alt: "GMTK Youtube Channel", src: youtubeIcon, href: "https://www.youtube.com/@GMTK"},
         {alt: "GMTK Game Jam page on itch.io", src: gmtkIcon, href: jamUrl},
-        {alt: "GMTK Discord server", src: discordIcon, href: "https://discord.gg/boss-rush-jam-839578524242149376"},
+        {alt: "GMTK Discord server", src: discordIcon, href: "https://discord.gg/uzKPF4NjfJ"},
     ]
 
     return (
