83ee071f4b589c4ca91633670fbe6bfc60014c9b
WillDotWhite <willcopsonwhite@gmail.com>
2026-01-30 12:59:10 +0000

    Set specific colours on Search and MyPost dropdowns

--- ui/src/pages/mypost/MyPost.tsx
+++ ui/src/pages/mypost/MyPost.tsx
@@ -105,71 +105,71 @@ const FieldDescription: React.FC<{ description: string}> = ({description}) => {
 
 const FieldSkillsPossessed: React.FC = () => {
     return (
-        <div>
-            <label htmlFor="skillsPossessed">What skills do you have?</label>
-            <Field
-                name="skillsPossessed"
-                className="c-dropdown form-block__field"
-                options={skills}
-                component={CustomSelect}
-                placeholder={"Select option(s)"}
-                isMulti={true}
-            />
-        </div>
+        <div id="skills-possessed-field">
+          <label htmlFor="skillsPossessed">What skills do you have?</label>
+          <Field
+              name="skillsPossessed"
+              className="c-dropdown form-block__field"
+              options={skills}
+              component={CustomSelect}
+              placeholder={"Select option(s)"}
+              isMulti={true}
+          />
+      </div>
     )
 }
 
 const FieldSkillsSought: React.FC = () => {
     return (
-        <div>
-            <label htmlFor="skillsSought">What skills are you looking for?</label>
-            <Field
-                name="skillsSought"
-                className="c-dropdown form-block__field"
-                options={skills}
-                component={CustomSelect}
-                placeholder={"Select option(s)"}
-                isMulti={true}
-            />
+        <div id="skills-sought-field">
+          <label htmlFor="skillsSought">What skills are you looking for?</label>
+          <Field
+              name="skillsSought"
+              className="c-dropdown form-block__field"
+              options={skills}
+              component={CustomSelect}
+              placeholder={"Select option(s)"}
+              isMulti={true}
+          />
         </div>
     )
 }
 
 const FieldLanguages: React.FC = () => {
     return (
-        <div>
-            <label htmlFor="languages">What languages do you speak?</label>
-            <Field
-                name="languages"
-                className="c-dropdown form-block__field"
-                options={languages}
-                component={CustomSelect}
-                placeholder={"Select option(s)"}
-                isMulti={true}
-            />
+      <div id="languages-field">
+          <label htmlFor="languages">What languages do you speak?</label>
+          <Field
+              name="languages"
+              className="c-dropdown form-block__field"
+              options={languages}
+              component={CustomSelect}
+              placeholder={"Select option(s)"}
+              isMulti={true}
+          />
         </div>
     )
 }
 
 const FieldTools: React.FC = () => {
     return (
-        <div>
-            <label htmlFor="preferredTools">What tools do you want to work with?</label>
-            <Field
-                name="preferredTools"
-                className="c-dropdown form-block__field"
-                options={tools}
-                component={CustomSelect}
-                placeholder={"Select option(s)"}
-                isMulti={true}
-            />
+      <div id="engines-field">
+          <label htmlFor="preferredTools">What tools do you want to work with?</label>
+          <Field
+              name="preferredTools"
+              className="c-dropdown form-block__field"
+              options={tools}
+              component={CustomSelect}
+              placeholder={"Select option(s)"}
+              isMulti={true}
+          />
         </div>
     )
 }
 
 const FieldTimezones: React.FC = () => {
     return (
-        <div>
+      <div id="timezones-field">
             <label htmlFor="timezoneOffsets">What timezone(s) are you working in?</label>
             <Field
                 name="timezoneOffsets"
